5cbc258f1f67eaa020e803ccf1d29f00049fbe02
JMustang <eecfredes@gmail.com>
2021-03-16 21:16:21 -0300

    terceira aula:metodos getter, setter e constructor

---
+++ aula03/src/aula03/Aula03.java
@@ -0,0 +1,9 @@
+package aula03;
+public class Aula03 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("FLIC","Amarela",0.4f);
+        c1.status();
+        
+    }
+    
+}
